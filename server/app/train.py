31455220092f4edf5621ed96eb9f551727c127a7
teanoon <anticipationvu@gmail.com>
2017-02-16 04:01:10 +0000

    re-arange code structure

--- server/app/train.py
+++ server/app/train.py
@@ -1,17 +1,15 @@
 import itertools
 import os
-import uuid
 
-from hyperopt import hp, fmin, tpe
-from hyperopt.mongoexp import MongoTrials
+from hyperopt import Trials, fmin, hp, tpe
 from tensorflow.examples.tutorials.mnist import input_data
 
 from server.app.service import SessionRunner
 
+DATA_SET = input_data.read_data_sets(os.path.join(os.path.dirname(__file__), '../resources'))
+
 
 def training(args):
-    data_set = input_data.read_data_sets(os.path.join(os.path.dirname(__file__), '../resources'))
-
     losses = []
     validation_accuracies = []
     with SessionRunner(args) as runner:
@@ -22,18 +20,18 @@ def training(args):
                 print('start epoch %d' % (epoch + 1))
 
             # train
-            images, labels = data_set.train.next_batch(runner.batch_size)
+            images, labels = DATA_SET.train.next_batch(runner.batch_size)
             _loss, _train = runner.process([loss, train], images, labels)
             losses.append(_loss)
 
             # validate
             if not step + 1 == runner.steps:
                 continue
-            validation_accuracy = runner.process(accuracy, data_set.validation.images, data_set.validation.labels)
+            validation_accuracy = runner.process(accuracy, DATA_SET.validation.images, DATA_SET.validation.labels)
             validation_accuracies.append(validation_accuracy)
 
         # objective value
-        test_accuracy = runner.process(accuracy, data_set.test.images, data_set.test.labels)
+        test_accuracy = runner.process(accuracy, DATA_SET.test.images, DATA_SET.test.labels)
         return 1 - test_accuracy
 
 
@@ -45,15 +43,15 @@ space = {
     'num_epochs_per_decay': hp.quniform('num_epochs_per_decay', 1, 20, 1)
 }
 
-# TODO use MongoTrials
-trials = MongoTrials('mongo://mongo:27017/mnist/jobs', exp_key='case-1')
-best = fmin(
-    training,
-    space=dict(space.items(), **{'example_size': 500}),
-    algo=tpe.suggest,
-    trials=trials,
-    max_evals=10)
-print(best)
-
+# TODO use mongo to store Trials
+trials = Trials()
+for step in range(3):
+    best = fmin(
+        training,
+        space=dict(space.items(), **{'example_size': 500}),
+        algo=tpe.suggest,
+        trials=trials,
+        max_evals=step+1)
+print(trials.trials)
 # final_errors = training(dict(best.items(), **{'example_size': 55000}))
 # print('test accuracy: %f' % (1 - final_errors))
